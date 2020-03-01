01f8d79b1dd29dc287d6bb9f089b7fcfda69f7d8
Maycon Brito <mayconfsbrito@gmail.com>
2020-03-01 10:44:03 -0300

    Average value in a double List

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,19 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      if (args.Length > 0)
-      {
-        Console.WriteLine($"Hello, {args[0]}!");
-      }
-      else
-      {
-        Console.WriteLine("Hello!");
-      }
+        static void Main(string[] args)
+        {
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+
+            var result = 0.0;
+            foreach (var number in grades)
+            {
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N3}");
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
+        }
     }
-  }
 }
