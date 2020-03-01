77fc1dc859cb53c1fb9814dbac7ab53b5484ddc4
Maycon Brito <mayconfsbrito@gmail.com>
2020-02-29 23:50:47 -0300

    Create parameter for names and to run in the debbuger mode

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -2,11 +2,18 @@
 
 namespace GradeBook
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      if (args.Length > 0)
+      {
+        Console.WriteLine($"Hello, {args[0]}!");
+      }
+      else
+      {
+        Console.WriteLine("Hello!");
+      }
     }
+  }
 }
