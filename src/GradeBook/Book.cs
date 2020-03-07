f9ff76231f67e0578686d933092c54c3fe19bb8d
Maycon Brito <mayconfsbrito@gmail.com>
2020-03-07 13:57:40 -0300

    OO, Files and Refactoring

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,16 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace GradeBook
 {
     public delegate void GradeAddedDelegate(object sender, EventArgs args);
 
-    public class Book
+    public interface IBook
     {
-        List<double> grades = new List<double>();
+        public void AddGrade(double grade);
+        Statistics GetStatistics();
+        string Name { get; }
+        event GradeAddedDelegate GradeAdded;
+    }
 
-        public event GradeAddedDelegate GradeAdded;
-
+    public class NamedObject
+    {
         private string name;
 
         public string Name
@@ -28,7 +33,66 @@ namespace GradeBook
             }
         }
 
-        public Book(string name)
+        public NamedObject(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public abstract class Book : NamedObject, IBook
+    {
+        public Book(string name) : base(name)
+        {
+        }
+
+        public virtual event GradeAddedDelegate GradeAdded;
+        public abstract void AddGrade(double grade);
+        public abstract Statistics GetStatistics();
+    }
+
+    public class DiskBook : Book
+    {
+        public DiskBook(string name) : base(name)
+        {
+
+        }
+        public override event GradeAddedDelegate GradeAdded;
+        public override Statistics GetStatistics()
+        {
+            var result = new Statistics();
+
+            using (var reader = File.OpenText($"{Name}.txt"))
+            {
+                var line = reader.ReadLine();
+                while (line != null)
+                {
+                    var number = double.Parse(line);
+                    result.Add(number);
+                    line = reader.ReadLine();
+                }
+            }
+
+            return result;
+        }
+
+        public override void AddGrade(double grade)
+        {
+            using (var writer = File.AppendText($"{Name}.txt"))
+            {
+                writer.WriteLine(grade);
+                if (GradeAdded != null)
+                    GradeAdded(this, new EventArgs());
+            }
+        }
+    }
+
+    public class InMemoryBook : Book
+    {
+        List<double> grades = new List<double>();
+
+        public override event GradeAddedDelegate GradeAdded;
+
+        public InMemoryBook(string name) : base(name)
         {
             Name = name;
         }
@@ -59,7 +123,7 @@ namespace GradeBook
             }
         }
 
-        public void AddGrade(double grade)
+        public override void AddGrade(double grade)
         {
             if (grade <= 100 && grade >= 0)
             {
@@ -75,42 +139,12 @@ namespace GradeBook
             }
         }
 
-        public Statistics GetStatistics()
+        public override Statistics GetStatistics()
         {
             var result = new Statistics();
-            result.Average = 0.0;
-            result.High = double.MinValue;
-            result.Low = double.MaxValue;
+
             foreach (var grade in grades)
-            {
-                result.Low = Math.Min(grade, result.Low);
-                result.High = Math.Max(grade, result.High);
-                result.Average += grade;
-            }
-            result.Average /= grades.Count;
-
-            switch (result.Average)
-            {
-                case var d when d >= 90.0:
-                    result.Letter = 'A';
-                    break;
-
-                case var d when d >= 80.0:
-                    result.Letter = 'B';
-                    break;
-
-                case var d when d >= 70.0:
-                    result.Letter = 'C';
-                    break;
-
-                case var d when d >= 60.0:
-                    result.Letter = 'D';
-                    break;
-
-                default:
-                    result.Letter = 'F';
-                    break;
-            }
+                result.Add(grade);
 
             return result;
         }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -7,12 +6,23 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var book = new Book("Maycon Brito");
+            IBook book = new DiskBook("Maycon Brito");
             book.GradeAdded += OnGradeAdded;
             book.GradeAdded += OnGradeAdded;
             book.GradeAdded -= OnGradeAdded;
             book.GradeAdded += OnGradeAdded;
+            EnterGrades(book);
 
+            var stats = book.GetStatistics();
+            Console.WriteLine($"For the book named {book.Name}");
+            Console.WriteLine($"The lowest grade is {stats.Low:N3}");
+            Console.WriteLine($"The highest grade is {stats.High:N3}");
+            Console.WriteLine($"The average grade is {stats.Average:N3}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
+        }
+
+        private static void EnterGrades(IBook book)
+        {
             while (true)
             {
                 Console.Write("Please, insert a grade for this book or 'q' to quit:");
@@ -38,13 +48,6 @@ namespace GradeBook
                     Console.WriteLine("**");
                 }
             }
-
-            var stats = book.GetStatistics();
-            Console.WriteLine($"For the book named {book.Name}");
-            Console.WriteLine($"The lowest grade is {stats.Low:N3}");
-            Console.WriteLine($"The highest grade is {stats.High:N3}");
-            Console.WriteLine($"The average grade is {stats.Average:N3}");
-            Console.WriteLine($"The letter grade is {stats.Letter}");
         }
 
         static void OnGradeAdded(object sender, EventArgs args)
