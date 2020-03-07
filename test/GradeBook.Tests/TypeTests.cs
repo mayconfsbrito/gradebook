3c7fc6255fd6c334e34f57d0d99a99368b2ced69
Maycon Brito <mayconfsbrito@gmail.com>
2020-03-07 14:17:19 -0300

    Fixing broken tests

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -60,21 +60,21 @@ namespace GradeBook.Tests
 
         private void GetBookSetName(ref Book book1, string name)
         {
-            book1 = new Book(name);
+            book1 = new InMemoryBook(name);
         }
 
         [Fact]
         public void CSharpIsPassByValue()
         {
             var book1 = GetBook("Book 1");
-            GetBookSetName(book1, "New Name");
+            GetBookSetName(ref book1, "New Name");
 
             Assert.Equal("Book 1", book1.Name);
         }
 
         private void GetBookSetName(Book book1, string name)
         {
-            book1 = new Book(name);
+            book1 = new InMemoryBook(name);
         }
 
         [Fact]
@@ -113,7 +113,7 @@ namespace GradeBook.Tests
 
         Book GetBook(string name)
         {
-            return new Book(name);
+            return new InMemoryBook(name);
         }
     }
 }
